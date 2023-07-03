ac2a5e1756ac6d3c49427f48391d76050c51891d
egorbekhterev <e.behterev@yandex.ru>
2023-07-03 13:34:13 +0700

    add keycloak support

---
+++ src/main/java/com/shoponline/inventoryservice/config/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.shoponline.inventoryservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.stream.Stream;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        httpSecurity.oauth2Login(Customizer.withDefaults());
+        return httpSecurity
+                .authorizeHttpRequests(error -> error.requestMatchers("/error").permitAll()
+                        .requestMatchers("/manager.html").hasRole("MANAGER")
+                        .anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var converter = new JwtAuthenticationConverter();
+        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        converter.setPrincipalClaimName("preferred_username");
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var authorities = jwtGrantedAuthoritiesConverter.convert(jwt);
+//            var roles = (List<String>) jwt.getClaimAsMap("realm_access").get("roles");
+            var roles = jwt.getClaimAsStringList("inventory-service-roles");
+            return Stream.concat(authorities.stream(),
+                    roles.stream()
+                            .filter(role -> role.startsWith("ROLE_"))
+                            .map(SimpleGrantedAuthority::new)
+                            .map(GrantedAuthority.class::cast))
+                    .toList();
+        });
+        return converter;
+    }
+
+    @Bean
+    public OAuth2UserService<OidcUserRequest, OidcUser> oAuth2UserService() {
+        var oidcUserService = new OidcUserService();
+        return userRequest -> {
+            var oidcUser = oidcUserService.loadUser(userRequest);
+            var roles = oidcUser.getClaimAsStringList("inventory-service-roles");
+            var authorities = Stream.concat(
+                    oidcUser.getAuthorities().stream(),
+                    roles.stream()
+                            .filter(role -> role.startsWith("ROLE_"))
+                            .map(SimpleGrantedAuthority::new)
+                            .map(GrantedAuthority.class::cast))
+                    .toList();
+            return new DefaultOidcUser(authorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+        };
+    }
+}
